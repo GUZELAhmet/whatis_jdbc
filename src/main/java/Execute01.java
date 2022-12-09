d75544bdab55eae69f9faffa1ad796bb6385a6f0
Ahmet Guzel <gzlhmt@gmail.com>
2022-12-09 23:48:05 +0300

    v2

--- src/main/java/Execute01.java
+++ src/main/java/Execute01.java
@@ -4,28 +4,41 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Execute01 {
-
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        //1. Adım: Driver'a kaydol
         Class.forName("org.postgresql.Driver");
-        Connection con = DriverManager.getConnection
-                ("jdbc:postgresql://localhost:5432/Techpro","postgres","1234");
-        Statement st = con .createStatement();
-        System.out .println ("Connection Succes");
 
+        //2. Adım: Datbase'e bağlan
+        Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","1234");
 
-        String sql1="create table workers(worker_id varchar(50),worker_name varchar(50),worker_salary int);";
-       st.execute(sql1);
+        //3. Adım: Statement oluştur.
+        Statement st = con.createStatement();
 
-       String sql2="Alter table workers add worker_adress varchar(80);";
-       st.execute(sql2);
+        //4. Adım: Query çalıştır.
 
-    String sql3="drop table workers;";
-    //st.execute(sql3);
+        /*
+        execute() methodu DDL(create, drop, alter table) ve DQL(select) için kullanılabilir.
+        1) Eğer execute() methodu DDL için kullanılırsa 'false' return yapar.
+        2) Eğer execute() methodu DQL için kullanılırsa ResultSet alındığında 'true' aksi hale 'false' verir.
+         */
 
+        //1.Örnek: "workers" adında bir table oluşturup "worker_id, worker_name, worker_salary" sütunlarını ekleyin.
 
+        boolean sql1 = st.execute("CREATE TABLE workers (worker_id VARCHAR(20), worker_name VARCHAR(20), worker_salary INT)");
+        System.out.println("sql1 = " + sql1);//false return eder çünkü data çağırmıyoruz.
+
+        //2.Örnek: Table'a worker_address sütunu ekleyerek alter yapın.
+        String sql2 = "ALTER TABLE workers ADD worker_address VARCHAR(80)";
+        boolean sql2b = st.execute(sql2);
+        System.out.println("sql2b = " + sql2b);
+
+        //3.Örnek: workers table'ını silin.
+        String sql3 = "DROP TABLE workers";
+        boolean sql3b = st.execute(sql3);
+        System.out.println("sql3b = " + sql3b);
+
+        //5. Adım: Bağlantı ve Statement'ı kapat.
         con.close();
         st.close();
-
-
     }
 }
